6ab0f08d697e42c53e9a7a493db69d517057777c
Lee Jeongil <jeongil.lee@gmail.com>
2025-05-27 11:17:24 +0900

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/azzu/traefik-maintenance
+
+go 1.24.3
---
+++ maintenance.go
@@ -0,0 +1,128 @@
+package traefik_maintenance
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"text/template"
+)
+
+type Config struct {
+	Enabled          bool   `json:"enabled"`
+	FileName         string `json:"file_name"`
+	TriggerUrl       string `json:"trigger_url"`
+	HttpResponseCode int    `json:"http_response_code"`
+	HttpContentType  string `json:"http_content_type"`
+}
+
+type MaintenancePage struct {
+	next             http.Handler
+	enabled          bool
+	fileName         string
+	triggerUrl       string
+	httpResponseCode int
+	httpContentType  string
+	name             string
+	template         *template.Template
+}
+
+func CreateConfig() *Config {
+	return &Config{
+		Enabled:          false,
+		FileName:         "",
+		TriggerUrl:       "",
+		HttpResponseCode: http.StatusOK,
+		HttpContentType:  "text/html; charset=utf-8",
+	}
+}
+
+func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	if len(config.FileName) == 0 {
+		return nil, fmt.Errorf("file_name is required")
+	}
+
+	return &MaintenancePage{
+		enabled:          config.Enabled,
+		fileName:         config.FileName,
+		triggerUrl:       config.TriggerUrl,
+		httpResponseCode: config.HttpResponseCode,
+		httpContentType:  config.HttpContentType,
+		next:             next,
+		name:             name,
+		template:         template.New("MaintenancePage").Delims("[[", "]]"),
+	}, nil
+}
+
+func (m *MaintenancePage) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if m.maintenanceEnabled() && m.triggerMaintenance(m.triggerUrl) {
+		if m.isURL(m.fileName) {
+			resp, err := http.Get(m.fileName)
+			if err != nil {
+				log.Printf("Error checking maintenance status: %v", err)
+				return
+			}
+			defer resp.Body.Close()
+			if resp.StatusCode == http.StatusOK {
+				w.Header().Set("Content-Type", m.httpContentType)
+				w.WriteHeader(m.httpResponseCode)
+				body, err := io.ReadAll(resp.Body)
+				if err != nil {
+					log.Printf("Error reading response body: %v", err)
+					return
+				}
+				w.Write(body)
+				return
+			} else {
+				log.Printf("Error checking maintenance status: %v", resp.StatusCode)
+				return
+			}
+		} else {
+			bytes, err := os.ReadFile(m.fileName)
+			if err != nil {
+				log.Printf("Error reading file: %v", err)
+				return
+			}
+			w.Header().Set("Content-Type", m.httpContentType)
+			w.WriteHeader(m.httpResponseCode)
+			w.Write(bytes)
+		}
+	}
+	m.next.ServeHTTP(w, r)
+}
+
+func (m *MaintenancePage) maintenanceEnabled() bool {
+	if !m.enabled {
+		return false
+	}
+
+	if m.enabled && len(m.triggerUrl) == 0 {
+		return true
+	}
+	if _, err := os.Stat(m.fileName); err == nil {
+		return true
+	}
+
+	return false
+}
+
+func (m *MaintenancePage) isURL(triggerUrl string) bool {
+	u, err := url.Parse(triggerUrl)
+	if err != nil {
+		return false
+	}
+	return u.Scheme != "" && u.Host != ""
+}
+
+func (m *MaintenancePage) triggerMaintenance(triggerUrl string) bool {
+	resp, err := http.Get(triggerUrl)
+	if err != nil {
+		log.Printf("Error triggering maintenance: %v", err)
+		return false
+	}
+	defer resp.Body.Close()
+	return resp.StatusCode == http.StatusOK
+}
